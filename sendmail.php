ff94cb2f2729cbe942199b8bfa6b6897ef9a5cf9
Marco García <marcogarcia.gon@gmail.com>
2018-05-30 19:00:27 -0500

    terminado

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+    $to = "marcogarcia.gon@gmail.com"; // this is your Email address
+    $from = $_POST['no-reply@quality.com.mx']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $subject = "Form submission";
